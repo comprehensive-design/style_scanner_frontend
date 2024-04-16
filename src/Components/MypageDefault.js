f8587351f6dbd155eb1687201c6b64eac3872a25
eunznidang <90143666+eunznidang@users.noreply.github.com>
2024-04-16 16:43:28 +0900

    마이페이지-기본화면 구현

--- src/Components/MypageDefault.js
+++ src/Components/MypageDefault.js
@@ -9,11 +9,84 @@ export default function MypageDefault() {
                 <div className='menuBar'>메뉴바입니다</div>
             </header>
 
-            <div className='sideBar'>사이드바입니다</div>
-            <div className='content'>
-                <div className='profileBox'>프로필박스</div>
-                <div className='followingBox'>팔로잉박스</div>
-                <div className='likeBox'>좋아요박스</div>
+            <div className='wrap'>
+                
+                <div className='sideBar'>사이드바입니다</div>
+
+                <div className='content'>
+                    <div className='profileBox'>
+                        <img className='profileImg' src="http://via.placeholder.com/100X100"></img>
+                        <div className='left'>
+                            <p id='userName'>username</p>
+                            <small id='userId'>userid</small>
+                        </div>
+                        <div className='right' id='followNum'>
+                            <p>followNum</p>
+                            <small>팔로잉</small>
+                        </div>
+                    </div>
+
+                    <div className='textBox'>
+                        <p>팔로잉</p>
+                        <p>더보기</p>
+                    </div>
+
+                    <div className='followingBox'>
+                        <div className='box'>
+                            <img className='followingImg' src="http://via.placeholder.com/100X100"></img>
+                            <p className='folowingID'>@celebID</p>
+                        </div>
+                        <div className='box'>
+                            <img className='followingImg' src="http://via.placeholder.com/100X100"></img>
+                            <p className='folowingID'>@celebID</p>
+                        </div>
+                        <div className='box'>
+                            <img className='followingImg' src="http://via.placeholder.com/100X100"></img>
+                            <p className='folowingID'>@celebID</p>
+                        </div>
+                        <div className='box'>
+                            <img className='followingImg' src="http://via.placeholder.com/100X100"></img>
+                            <p className='folowingID'>@celebID</p>
+                        </div>
+                    </div>
+
+                    <div className='textBox'>
+                        <p>좋아요</p>
+                        <p>더보기</p>
+                    </div>
+
+                    <div className='likeBox'>
+                        <div className='boxes'>
+                            <img className='boxComponent' id='itemImg' src="http://via.placeholder.com/100X100"></img>
+
+                            <p className='boxComponent' id='brandName'>브랜드이름</p>
+                            <div className='itemBox'>
+                                <p id='itemName'>아이템이름</p> -
+                                <p id='itemOption'>옵션명</p>
+                            </div>
+                            <p className='boxComponent' id='price'>가격</p>
+                            <div className='heartBox'>
+                                <img id='heartImg' src="img/fullHeart.png"></img>
+                                <p id='likeCount'>좋아요수</p>
+                            </div>
+                        </div>
+                        <div className='boxes'>
+                            <img className='boxComponent' id='itemImg' src="http://via.placeholder.com/100X100"></img>
+
+                            <p className='boxComponent' id='brandName'>브랜드이름</p>
+                            <div className='itemBox'>
+                                <p id='itemName'>아이템이름</p> -
+                                <p id='itemOption'>옵션명</p>
+                            </div>
+                            <p className='boxComponent' id='price'>가격</p>
+                            <div className='heartBox'>
+                                <img id='heartImg' src="img/fullHeart.png"></img>
+                                <p id='likeCount'>좋아요수</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </body>
     );
