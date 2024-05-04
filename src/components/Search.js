feaeafe7b2aeeae883bcd39ef282a5b2f7f25c6d
eunnnju <sbsdy3@gmail.com>
2024-05-04 15:59:06 +0900

    search 창 수정

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,39 @@
 import styles from './Search.css';
 import HorizonLine  from '../utils/HorizontalLine';
 import Box from '@material-ui/core/Box';
-
-
+import './Search.css';
 
 function Search(){
     return(
-        <div className={styles.parent}>
+        <div style={{display:'flex'}}>
             
-            <div className={styles.child}>
+            <div style={{margin: 'auto auto'}} id="profileBox">
                 <p>전체</p>
                 <Box class = "box1">
 
-                    <img
-                        class = "profile"
-                        src = "https://via.placeholder.com/200x200/808080/FFFFFF/?text=Grey+Image"
-                    />
+                    <div style ={{display:'flex'}}>
+                        <Box style={{width:50}}></Box>
+
+                        <img
+                            className = "profileImg"
+                            src = "https://via.placeholder.com/150x150/808080/FFFFFF/?text=Grey+Image"
+                        />
+
+                        <div className = "profileId">
+                            <p id="userid">hi_sseulgi</p>
+                            <p>팔로워 465</p>
+                        </div>
+
+                        {/* <Box style={{width:500}}/> */}
+                        <button class="button"></button>
+
+                        
+                    </div>
+
+                    
+
+
+                    
                 </Box>
 
 
