3a6fbad2492e9f6e1300618af55b4c56fce13db3
wlstj <22020306jinseoyeon@gmail.com>
2024-05-06 19:31:22 +0900

    ItemInfo 무한스크롤 구현

--- src/Components/HomeInfo.js
+++ src/Components/HomeInfo.js
@@ -1,9 +1,45 @@
+import React, { useState, useEffect, useRef } from 'react';
 import Feed from './feed.js';
 import ItemInfo from './ItemInfo.js';
 import '../css/HomeInfo.css';
 
 export default function HomeInfo(){
-    
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const itemListRef = useRef(); 
+   
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (!entry.isIntersecting) {
+                    return;
+                }
+                if (loading) return;
+                
+                loadItems();
+            });
+        });
+        observer.observe(itemListRef.current);
+
+        return () => {
+            observer.disconnect();
+        };
+
+    }, [loading]); 
+
+    const loadItems = () => {
+        setLoading(true); 
+        const newItems = [];
+        for (let i = 0; i < 3; i++) {
+            newItems.push({
+                id: items.length + i,
+                image: ' '
+            });
+        }
+        setItems([...items, ...newItems]);
+        setLoading(false); 
+    };
+
     return(
         <body>
             <header>
@@ -12,12 +48,12 @@ export default function HomeInfo(){
 
             <div className="contents">
                 <Feed></Feed>
-                <div className="totalItem">
+                <div className="totalItem" ref={itemListRef}>
                     {/* item 정보  */}
-                    <ItemInfo></ItemInfo>
-                    <ItemInfo></ItemInfo>
-                    <ItemInfo></ItemInfo>
-                    
+                    {items.map(feed => (
+                    <ItemInfo key={feed.id} image={feed.image} />
+                    ))}
+                    <div style={{height: '10px'}} />
                 </div>
                 <div className="decoBox"></div>
                
