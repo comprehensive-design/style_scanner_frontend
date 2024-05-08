6ba25a7f40c9d5009aa80a3b91f5e88c732d8c84
eunnnju <sbsdy3@gmail.com>
2024-05-08 13:16:46 +0900

    category update

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,5 +1,6 @@
 // import { Category } from "@mui/icons-material";
 import '../css/Category.css';
+// import HorizonLine from '../utils/HorizontalLine';
 import {NavLink} from "react-router-dom";
 
 export default function Category() {
@@ -10,57 +11,57 @@ export default function Category() {
     return(
         <div id = "categoryDiv">
             <h2 id="categoryNa">카테고리</h2>
-
+            <HorizonLine></HorizonLine>
             
             <div id="categoryList">
                 <ul>
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className="gen">
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className="gen lists">
                         여성
                     </NavLink >
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = ''>
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className = "lists">
                         상의
                     </NavLink >
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = ''>
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className = "lists">
                         팬츠
                     </NavLink >
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = ''>
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className = "lists">
                         스커트
                     </NavLink >
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = ''>
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className = "lists">
                         원피스
                     </NavLink >
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = ''>
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className = "lists">
                         악세사리
                     </NavLink >
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = ''>
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className = "lists lastlists">
                         기타
                     </NavLink >
 
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className="gen">
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className="gen lists">
                         남성
                     </NavLink >
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = ''>
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className = "lists">
                         상의
                     </NavLink >
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = ''>
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className = "lists">
                         팬츠
                     </NavLink >
 
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = ''>
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className = "lists">
                         악세사리
                     </NavLink >
 
-                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = ''>
+                    <NavLink style={({isActive}) => (isActive? activeStyle : {})} to = '' className = "lists">
                         기타
                     </NavLink >
 
@@ -71,3 +72,17 @@ export default function Category() {
     )
 }
 
+const HorizonLine = () => {
+    return (
+      <div
+        style={{
+          width: "100%",
+          borderBottom: "2px solid #aaa",
+          lineHeight: "0.1em",
+          margin: "10px 0 20px",
+        }}
+      >
+      </div>
+    );
+  };
+  
