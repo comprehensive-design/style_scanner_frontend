939b37b90c436f75d2b15c3b66e9ae710f61bce7
wlstj <22020306jinseoyeon@gmail.com>
2024-05-08 14:30:07 +0900

    community Feed ui, comFeed 컴포넌트 파일 추가

--- src/App.js
+++ src/App.js
@@ -3,17 +3,16 @@ import Main from './Components/Main';
 import Register from './Components/Register';
 import Login from './Components/Login';
 import MypageDefault from './Components/MypageDefault';
-import Feed from './Components/feed.js';
+import CommunityFeed from './Components/CommunityFeed';
 const App = () => {
   return (
-    // <Routes>
-    // <Route path="/" element={<Main />} />
-    //   <Route path="/Register" element={<Register />} />
-    //   <Route path="/Login" element={<Login />} />
-    //   <Route path="/MypageDefault" element={<MypageDefault />} />
-      
-    // </Routes>
-    <Feed></Feed>
+    <Routes>
+    <Route path="/" element={<Main />} />
+      <Route path="/Register" element={<Register />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/MypageDefault" element={<MypageDefault />} />
+      <Route path="/CommunityFeed" element={<CommunityFeed />} />
+    </Routes>
   );
 };
 
---
+++ src/Components/CommunityFeed.js
@@ -0,0 +1,12 @@
+import '../css/CommunityFeed.module.css'
+
+export default function CommunityFeed() {
+    return(
+        <div>
+            <header>
+                <div className='menuBar'>메뉴바입니다</div>
+            </header>
+            
+        </div>
+    );
+}
