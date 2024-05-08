da8b1d6d3752d48bfc6b26481e0410342bbdc88c
wlstj <22020306jinseoyeon@gmail.com>
2024-05-08 19:49:10 +0900

    질문 글 css 수정

--- src/Components/comfeed.js
+++ src/Components/comfeed.js
@@ -9,7 +9,8 @@ function comFeed(){
             <div className={styles.writeBox}>
                 <span className={styles.writeId} id="writerId"><b>useruser2</b></span>
                 <div className={styles.writeTotal}>
-                    <span className={styles.writeContent} id="writeContent">속눈썹 궁금해..&nbsp;</span>
+                    {/* 내용 /태그 포함 25자 보여주기..*/}
+                    <span className={styles.writeContent} id="writeContent">속눈썹 궁금해요요요요요요요&nbsp;</span>
                     <span className={styles.tag} id="tag">@noodle.zip</span>
                 </div>
             </div>
