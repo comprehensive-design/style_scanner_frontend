cf947af8d6916bcbfbf919a0942e4bb833b9cb46
이은진 <ejejnin@gmail.com>
2024-05-10 14:58:37 +0900

    커스텀 버튼 수정

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,22 +3,24 @@ import styled from 'styled-components'
 
 const StyledButton = styled.p`
   display: inline-block;
+  min-width: 40px;
+  cursor: pointer;
+  padding: 7px 14px;
+  font-size: 12px;
   background-color: ${props => props.BackColor};
   color: ${props => props.txtColor};
-  min-width: 50px;
-  cursor: pointer;
-  border-radius: 10px;
-  padding: 10px 15px;
-  font-size: 12px;
+  border-radius: ${props => props.borderRad};
+  border: ${props => props.border};
 
   &:hover {
     background-color: ${props => props.hovColor};
+    color: ${props => props.hovTxtColor};
   }
 `;
 
-function Button({ onClick, children, BackColor='black', txtColor='white', hovColor='gray' }) {
+function Button({ onClick, children, BackColor='black', txtColor='white', hovColor='#d9d9d9' , border='0px', hovTxtColor='black', borderRad='5px'} ) {
   return (
-    <StyledButton onClick={onClick} BackColor={BackColor} txtColor={txtColor} hovColor={hovColor}>
+    <StyledButton onClick={onClick} BackColor={BackColor} txtColor={txtColor} hovColor={hovColor} hovTxtColor={hovTxtColor} border={border}  borderRad={borderRad}>
       {children}
     </StyledButton>
   );
