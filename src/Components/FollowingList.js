704feaa2583f57ade3fda9f32b70cedfd036044f
eunnnju <sbsdy3@gmail.com>
2024-05-11 14:43:58 +0900

    followinglist 수정 - 동적 컴포넌트 무한 스크롤 기능 추가

--- src/Components/FollowingList.js
+++ src/Components/FollowingList.js
@@ -1,8 +1,43 @@
 import styles from '../css/FollowingList.module.css';
 import Sidebar from "./Sidebar"
-import FollowingUser from './FollowingUser';
+import FollowingUser from "./FollowingUser";
+import React, {useState, useEffect, useRef} from 'react';
 
 export default function FollowingList() {
+    const [followings, setFollowings] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const followingListRef = useRef(); 
+   
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (!entry.isIntersecting) {
+                    return;
+                }
+                if (loading) return;
+                
+                loadFeeds();
+            });
+        });
+        observer.observe(followingListRef.current);
+
+        return () => {
+            observer.disconnect();
+        };
+    }, [loading]); 
+    const loadFeeds = () => {
+        setLoading(true); 
+        const newFollowings = [];
+        for (let i = 0; i < 5; i++) {
+            newFollowings.push({
+                id: followings.length + i,
+                image: `img/following${(followings.length + i) % 5 + 1}.png`
+            });
+        }
+        setFollowings([...followings, ...newFollowings]);
+        setLoading(false); 
+    };
+
     return(
         <body>
             <div style={{display:'flex'}}>
@@ -15,13 +50,15 @@ export default function FollowingList() {
                         <p>22</p>
                     </div>
 
-                    <div>
-                        <FollowingUser></FollowingUser>
+                    <div className="feedList" ref={followingListRef}>
+                        {followings.map(feed => (
+                            <FollowingUser key={followings.id} image={FollowingUser.image} />
+                         ))}
                     </div>
                 </div>
             </div>
         </body>
-    )
+    );
 }
 
 const HorizonLine = () => {
--- src/Components/FollowingUser.js
+++ src/Components/FollowingUser.js
@@ -1,23 +1,37 @@
 import styles from"../css/FollowingUser.module.css";
 
-export default function FollowingUser(){
+function FollowingUser(){
     return(
         <body>
+            <div style={{height : '12px'}}></div>
+
             <div class={styles.userInfo}>
                 <img
-                    id='FollowingUserImg'
-                    src={process.env.PUBLIC_URL + 'img/profileimg.png'}
+                    id={styles.profileImage}
+                    // src={process.env.PUBLIC_URL + 'img/profile.png'}
+                    src=" http://via.placeholder.com/200x200"
+                    width={200}
+                    height={200}
                 >
                 </img>
 
                 <div className={styles.userInfoWord}>
-                    <h4>hi_sseulgi</h4>
+                    <h4 className={styles.FollowigId}>hi_sseulgi</h4>
                     <div style={{display:"flex"}} className={styles.userFollowerInfo}>
-                        <p id="FollowerWord">팔로워</p>
-                        <p id="FollowerCountWord">434</p>
+                        <p id={styles.FollowerWord}>팔로워</p>
+                        <p id={styles.FollowerCountWord}>434</p>
                     </div>
                 </div>
+
+                <div style={{display:'flex'}}className={styles.FollowingDelete}>
+                    <button id={styles.buttonDelete}>삭제</button>
+                </div>
             </div>
+
+            <div style={{height : '12px'}}></div>
+            <hr></hr>
         </body>
     )
 }
+
+export default FollowingUser;
