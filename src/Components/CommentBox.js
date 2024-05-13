4a0ebb499af5949ef3244e4c8df6d07ba18b0e59
wlstj <22020306jinseoyeon@gmail.com>
2024-05-13 20:06:49 +0900

    MyPageComments json 데이터 불러오기

--- src/Components/CommentBox.js
+++ src/Components/CommentBox.js
@@ -1,7 +1,7 @@
 import styles from '../css/CommentBox.module.css';
 import Button from './Button';
 
-export default function CommentBox(){
+export default function CommentBox({feedId, title, contents, date }){
     
     const editClick = () => {
        alert("edit버튼 누름")
@@ -13,15 +13,15 @@ export default function CommentBox(){
         <div className={styles.writingBox}>
             <div className={styles.feedDiv}>
                 <img className={styles.feedImage} src="http://via.placeholder.com/150X200"></img>
-                <p className={styles.feedId}>@roses_are_rosie</p>
+                <p className={styles.feedId}>{feedId}</p>
             </div>
             <div className={styles.textDiv}>
-                <p className={styles.title}>로제 반지 어디 건가요?</p>
-                <p className={styles.contents}>까르띠에입니다!!!!! </p>
+                <p className={styles.title}>{title}</p>
+                <p className={styles.contents}>{contents} </p>
             </div>
         
             <div className={styles.dateDiv}>
-                <p className={styles.date}>2024.04.06</p>
+                <p className={styles.date}>{date}</p>
             </div>
             <div className={styles.buttonDiv}>
                 <Button onClick={editClick} BackColor="#d9d9d9" txtColor='black' border='none' hovColor='black' hovTxtColor='white'>수정</Button>
--- src/Components/MyPageComments.js
+++ src/Components/MyPageComments.js
@@ -1,20 +1,46 @@
+import { useState, useEffect } from 'react';
 import styles from "../css/MyPageComments.module.css";
 import Sidebar from './Sidebar';
 import CommentBox from './CommentBox';
+import axios from 'axios';
 
-export default function MyPageComments(){
-    return (
-        <body>
-            <Sidebar></Sidebar>
-            <div className={styles.content}>
-                <div className={styles.title}>
-                    <h3>내가 작성한 댓글</h3>
-                    <hr></hr>
-                </div>
-                <CommentBox/>
-                <CommentBox/>
-                <CommentBox/>
-            </div>
-        </body>
-    );
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
+
+export const getPosts = async () => {
+  const response = await axios.get("/posts");
+  return response.data;
+};
+
+export default function MyPageComments() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const data = await getPosts();
+        setPosts(data);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <body>
+      <Sidebar></Sidebar>
+      <div className={styles.content}>
+        <div className={styles.title}>
+          <h3>내가 작성한 댓글</h3>
+          <hr></hr>
+        </div>
+        {/* 5개씩 보여주고, 페이지네이션으로 넘기기 */}
+        {Array.isArray(posts)  && posts.slice(0, 5).map((post, index) => (
+        //   <CommentBox key={index} feedId= "@roses_are_rosie" title={post.title} contents={post.body} date="2024.05.13"/>
+            <CommentBox key={index} feedId= "@roses_are_rosie" title="로제 반지 어디 건가요?" contents="까르띠에입니다!!!!! " date="2024.05.13"/>
+        ))}
+      </div>
+    </body>
+  );
 }
