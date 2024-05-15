04dda87903cfa8810ba773ab07d05e56377d5b8e
eunnnju <sbsdy3@gmail.com>
2024-05-15 16:40:08 +0900

    category 기능 설정 - ranking도 같이 변경

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,12 +1,11 @@
-import styles from '../css/Category.module.css';
-import {Link} from "react-router-dom";
-import React, {useState} from 'react';
+ import styles from '../css/Category.module.css';
+import React, { useEffect, useState } from 'react';
 import Ranking from './Ranking';
 import styled from 'styled-components';
 
 export default function Category() {
     const activeStyle = {
-        color : 'black'
+        color: 'black'
     };
 
     const [selectedCategory, setSelectedCategory] = useState('전체');
@@ -15,67 +14,71 @@ export default function Category() {
 
     const categories = {
         '전체': [],
-        '여성': ['아우터', '상의', '팬츠', '스커트', '원피스', '신발', '가방','악세사리', '기타'],
+        '여성': ['아우터', '상의', '팬츠', '스커트', '원피스', '신발', '가방', '악세사리', '기타'],
         '남성': ['아우터', '상의', '팬츠', '신발', '가방', '악세사리', '기타']
     };
 
+    useEffect(() => {
+        // selectedCategory나 selectedSubcategory가 변경될 때마다 호출되는 부분
+    }, [selectedCategory, selectedSubcategory]);
+
     const handleCategoryClick = (category) => {
-      setSelectedCategory(category);
-      setSelectedSubcategory(''); // 상위 카테고리를 클릭했을 때 하위 카테고리 리셋
-  };
+        setSelectedCategory(category);
+        setSelectedSubcategory(''); // 상위 카테고리를 클릭했을 때 하위 카테고리 리셋
+    };
 
-  const handleSubcategoryClick = (subcategory) => {
-      setSelectedSubcategory(subcategory);
-    //   setActiveSortButton(subcategory); // 활성화된 버튼으로 설정
-  };
-    
-  const items = [
-      { name: 'Item 1', category: '전체' },
-      { name: 'Item 2', category: '여성' },
-      { name: 'Item 3', category: '여성', subcategory: '아우터' },
-      { name: 'Item 4', category: '여성', subcategory: '상의' },
-      { name: 'Item 5', category: '여성', subcategory: '팬츠' },
-      { name: 'Item 6', category: '여성', subcategory: '스커트' },
-      { name: 'Item 7', category: '여성', subcategory: '원피스' },
-      { name: 'Item 8', category: '여성', subcategory: '신발' },
-      { name: 'Item 9', category: '여성', subcategory: '가방' },
-      { name: 'Item 10', category: '여성', subcategory: '악세사리' },
-      { name: 'Item 11', category: '여성', subcategory: '기타' },
+    const handleSubcategoryClick = (subcategory) => {
+        setSelectedSubcategory(subcategory);
+        //   setActiveSortButton(subcategory); // 활성화된 버튼으로 설정
+    };
 
-      { name: 'Item 12', category: '남성' },
-      { name: 'Item 13', category: '남성', subcategory: '아우터' },
-      { name: 'Item 14', category: '남성', subcategory: '상의' },
-      { name: 'Item 15', category: '남성', subcategory: '팬츠' },
-      { name: 'Item 16', category: '남성', subcategory: '신발' },
-      { name: 'Item 17', category: '남성', subcategory: '가방' },
-      { name: 'Item 18', category: '남성', subcategory: '악세사리' },
-      { name: 'Item 19', category: '남성', subcategory: '기타' },
-];
+    const items = [
+        { name: 'Item 1', category: '전체' },
+        { name: 'Item 2', category: '여성' },
+        { name: 'Item 3', category: '여성', subcategory: '아우터' },
+        { name: 'Item 4', category: '여성', subcategory: '상의' },
+        { name: 'Item 5', category: '여성', subcategory: '팬츠' },
+        { name: 'Item 6', category: '여성', subcategory: '스커트' },
+        { name: 'Item 7', category: '여성', subcategory: '원피스' },
+        { name: 'Item 8', category: '여성', subcategory: '신발' },
+        { name: 'Item 9', category: '여성', subcategory: '가방' },
+        { name: 'Item 10', category: '여성', subcategory: '악세사리' },
+        { name: 'Item 11', category: '여성', subcategory: '기타' },
 
-const SortButton = styled.button` 
+        { name: 'Item 12', category: '남성' },
+        { name: 'Item 13', category: '남성', subcategory: '아우터' },
+        { name: 'Item 14', category: '남성', subcategory: '상의' },
+        { name: 'Item 15', category: '남성', subcategory: '팬츠' },
+        { name: 'Item 16', category: '남성', subcategory: '신발' },
+        { name: 'Item 17', category: '남성', subcategory: '가방' },
+        { name: 'Item 18', category: '남성', subcategory: '악세사리' },
+        { name: 'Item 19', category: '남성', subcategory: '기타' },
+    ];
+
+    const SortButton = styled.button`
     color: ${props => (props.active ? 'black' : 'rgb(153, 153, 153)')};
     background-color: transparent;
     text-decoration: none;
     border: none;
     cursor: pointer;
     // font-weight: ${props => (props.active ? 'bold' : 'normal')};
-`;
+    `;
 
-    return(
-        <div id = {styles.categoryDiv}>
+    return (
+        <div id={styles.categoryDiv}>
             <p id={styles.categoryNa}>카테고리</p>
             <HorizonLine></HorizonLine>
-      
+
             <div id={styles.categoryList}>
-            <ul className={styles.categoryUl}>
+                <ul className={styles.categoryUl}>
                     {Object.keys(categories).map((category, index) => (
                         <li key={index} className={styles.suplists}>
                             <SortButton onClick={() => {
-                                handleCategoryClick(category); 
+                                handleCategoryClick(category);
                                 setActiveSortButton('');
                             }}
-                            active = {selectedCategory === category}
-                            className={`${selectedCategory === category ? 'itemLinkOn' : 'itemLink'}  ${styles.supcateButton}`}>{category}</SortButton>
+                                active={selectedCategory === category}
+                                className={`${selectedCategory === category ? 'itemLinkOn' : 'itemLink'}  ${styles.supcateButton}`}>{category}</SortButton>
                             {selectedCategory === category &&
                                 <ul className={styles.subcategoryUl}>
                                     {categories[category].map((subcategory, subIndex) => (
@@ -83,9 +86,9 @@ const SortButton = styled.button`
                                             <SortButton onClick={() => {
                                                 handleSubcategoryClick(subcategory);
                                                 setActiveSortButton(subcategory);
-                                            }} 
-                                            active={selectedSubcategory === subcategory}
-                                            className={`${selectedSubcategory === subcategory ? 'itemLinkOn' : 'itemLink'} ${styles.subcateButton}`}>{subcategory}</SortButton>
+                                            }}
+                                                active={selectedSubcategory === subcategory}
+                                                className={`${selectedSubcategory === subcategory ? 'itemLinkOn' : 'itemLink'} ${styles.subcateButton}`}>{subcategory}</SortButton>
                                         </li>
                                     ))}
                                 </ul>
@@ -95,7 +98,8 @@ const SortButton = styled.button`
                 </ul>
 
             </div>
-            {/* <Ranking selectedCategory={selectedCategory} selectedSubcategory={selectedSubcategory} /> */}
+            {/* <Ranking selectedCategory={{selectedCategory}} selectedSubcategory = {selectedSubcategory}></Ranking> */}
+            {selectedCategory && <Ranking selectedCategory={selectedCategory} selectedSubcategory={selectedSubcategory} />}
 
         </div>
     )
@@ -103,15 +107,14 @@ const SortButton = styled.button`
 
 const HorizonLine = () => {
     return (
-      <div
-        style={{
-          width: "80%",
-          borderBottom: "2px solid #aaa",
-          lineHeight: "0.1em",
-          margin: "10px 0 20px",
-        }}
-      >
-      </div>
+        <div
+            style={{
+                width: "80%",
+                borderBottom: "2px solid #aaa",
+                lineHeight: "0.1em",
+                margin: "10px 0 20px",
+            }}
+        >
+        </div>
     );
-  };
-  
+};
--- src/Components/Ranking.js
+++ src/Components/Ranking.js
@@ -2,26 +2,38 @@ import Category from "./Category"
 import styles from '../css/Ranking.module.css';
 import { Link } from "react-router-dom";
 import RankingFeed from "./RankingFeed";
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 // https://leedaeho1188.tistory.com/55
 
-export default function Ranking(){
-    const [bestFeeds, setBestFeeds] = useState([]); 
+export default function Ranking({selectedCategory, selectedSubcategory}){
+    const [items, setItems] = useState([]);
+    const [itemsPerPage, setItemsPerPage] = useState(40); 
 
-    const loadFeeds = () => {
-        const newBestFeeds = [];
-        for (let i=0; i< 4; i++){
-            newBestFeeds.push({
-                id : bestFeeds.length + i,
-                image: `img/bestFeed${(bestFeeds.length+i)%4+1}.png`
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                setItems(response.data);
+            })
+            .catch((error) => {
+                // 에러 처리
+                console.error('데이터를 가져오는 중에 오류가 발생했습니다:', error);
             });
-        }
-        setBestFeeds([...bestFeeds, ...newBestFeeds]);
-    };
+    }, []);
+
+    // const renderItems = () => {
+    //     const items = [];
+    //     for (let i=0; i<4; i++){
+    //         items.push(
+    //             <RankingFeed key = {`feed${i}`}/>
+    //         );
+    //         items.push(<div key={`padding${i}`} className={styles.RankingPadding}></div>);
+    //     }
+    //     return items;
+    // }
 
     return(
-        
         <div className={styles.totalWrap}>
             <div className={styles.RankingWord}>
                 <h1 id={styles.rankingBest}>BEST</h1>
@@ -29,7 +41,7 @@ export default function Ranking(){
             </div>
 
             <div className={styles.ContentWrap} style={{display:'flex'}}>
-                <Category></Category>
+                {/* <Category></Category> */}
                 <div className={styles.RankingContent}>
                     <div className={styles.RankingType}>
                         <ul className={styles.RankingUl}>
@@ -37,108 +49,13 @@ export default function Ranking(){
                             <li className={styles.RankingLists}><Link>일간</Link></li>
                             <li className={styles.RankingLists}><Link>월간</Link></li>
                         </ul>
-                    </div>
 
-                    <div className={styles.RankingRow}>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        
+                        <div>
+                            {/* {renderItems()} */}
+                            <h1>test</h1>
+                        </div>
                     </div>
-
-                    <div className={styles.RankingRow}>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                    </div>
-
-                    <div className={styles.RankingRow}>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                    </div>
-
-                    <div className={styles.RankingRow}>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                    </div>
-
-                    <div className={styles.RankingRow}>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                    </div>
-
-                    <div className={styles.RankingRow}>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                    </div>
-
-                    <div className={styles.RankingRow}>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                    </div>
-
-                    <div className={styles.RankingRow}>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                    </div>
-
-                    <div className={styles.RankingRow}>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                    </div>
-
-                    <div className={styles.RankingRow}>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                        <div className={styles.RankingPadding}></div>
-                        <RankingFeed></RankingFeed>
-                    </div>
+                    
                 </div>
             </div>
         </div>
