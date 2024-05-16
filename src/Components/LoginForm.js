5fb52763ad6136688a1330e9f08dcffa5c9847a6
이은진 <ejejnin@gmail.com>
2024-05-16 10:18:50 +0900

    [로그인] label htmlFor 설정

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -10,15 +10,17 @@ export default function Register({email, password, setEmail, setPassword, handle
                 <p> 당신의 취향을 만들어 드릴게요! </p>
 
                 <form onSubmit={handleSubmit}>
-                    <label for="email" >이메일</label>
-                    <input type="text" name="email"  value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <label htmlFor="email" >이메일</label>
+                    <input type="text" id="email"  value={email}
+                    onChange={(e) => setEmail(e.target.value)}/>
 
-                    <label for="password" >비밀번호</label>
-                    <input type="password" name="password" value={password}
-                onChange={(e) => setPassword(e.target.value)}/>
+                    <label htmlFor="password" >비밀번호</label>
+                    <input type="password" id="password" value={password} 
+                    onChange={(e) => setPassword(e.target.value)}/>
 
                     <input className={styles.submitButton} type="submit" value="로그인"></input>
                 </form>
+                
                 <div style={{textAlign:'right', width: '70%', margin: '0 auto' }}>
                     <NavLink exact to='/Register' style={{fontSize:'14px'}}>회원가입</NavLink>
                 </div>
