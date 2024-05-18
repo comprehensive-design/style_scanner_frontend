1053d255cea76bb75c060912c203700ce33aba84
eunjin <ejejnin@gmail.com>
2024-05-18 17:09:24 +0900

    [기능 구현] 커뮤니티 본문과 링크 설정 #61

--- src/Components/NotiBox.js
+++ src/Components/NotiBox.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import styles from '../css/NotiBox.module.css';
 
-function NotiBox( {title="로제 폰케이스 뭔가요?", reply="저도 너무 궁금해요.....", date="2024.04.05"}) {
+function NotiBox( { link="http://localhost:3000/CommunityInfo", title="로제 폰케이스 뭔가요?", reply="저도 너무 궁금해요.....", date="2024.04.05"}) {
 
     return (
         <div>
             <div className={styles.box}>
                 <div className={styles.NBox}>
                     <div>
-                        <p className={styles.Q}>Q.&nbsp;</p>
-                        <p className={styles.Qtext} >{title}</p>
+                        <a href={link} className={styles.Qtext} >Q {title}</a>
                     </div>
                     <div>
                         <p className={styles.A}>A.&nbsp;</p>
