2690b193c1af9bf0a6c87df50d32413998e97bd1
이은진 <ejejnin@gmail.com>
2024-05-21 10:59:52 +0900

    [Feat] 로그인 백엔드 서버 연결

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,33 +1,42 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import LoginForm from './LoginForm';
 
 export default function Register() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:8080/login', {
-                email,
-                password,
-            });
-            console.log(response.data);
-            alert('로그인되었습니다!');
-            navigate('/homefeed'); 
+
+            if (email != '' && password != '') {
+                const response = await axios.post('http://54.180.208.255:9000/api/user/login', {
+                    "email": email,
+                    "password": password
+                });
+                const accessToken = response.data.accessToken;
+                localStorage.setItem("accessToken", accessToken);
+                console.log(response.data);
+                alert('로그인되었습니다!');
+                // navigate('/homefeed');
+                window.location.replace("/homefeed");
+            }
+            else {
+                alert('이메일 혹은 비밀번호를 입력해 주세요.');
+            }
         } catch (error) {
             alert('이메일 혹은 비밀번호를 확인해 주세요.', error);
         }
     };
 
     return (
-            <LoginForm
-                email={email} setEmail={setEmail}
-                password={password} setPassword={setPassword}
-                handleSubmit={handleSubmit}
-            />
+        <LoginForm
+            email={email} setEmail={setEmail}
+            password={password} setPassword={setPassword}
+            handleSubmit={handleSubmit}
+        />
     );
 }
