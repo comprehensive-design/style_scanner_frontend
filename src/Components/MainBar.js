7d5adc4e91c6a2695948b1c0e2eb3cbf6e38e992
이은진 <ejejnin@gmail.com>
2024-05-24 16:25:16 +0900

    [UI] 로그인 상태에 따른 메인바 css 수정

--- src/Components/MainBar.js
+++ src/Components/MainBar.js
@@ -1,41 +1,48 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
 import styles from '../css/MainBar.module.css';
 
+class MainBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: localStorage.getItem('accessToken') !== null
+        };
+    }
 
-function MainBar(){
-    const activeStyle = {
-        color : 'black'
-    };
+    render() {
+        return (
+            <header className={styles.header}>
+                <div style={{ display: 'flex' }} className={styles.parent}>
+                    <Link to="">
+                        <img
+                            src={`img/logo.png`}
+                            width='130'
+                            height='48.75'
+                            alt="Logo"
+                        />
+                    </Link>
+                    <SearchBar />
 
-    return(
-        <header className = {styles.header}>
-            <div style={{display:'flex'}} className ={styles.parent}>
-                <Link to="">
-                    <img
-                    
-                    src={`img/logo.png`}
-                    width = '130'
-                    height = '48.75'
-                    alt = "Logo"
-                    />
-                </Link>
-                <SearchBar></SearchBar>
+                    <nav className={styles.navigation}>
+                        <ul className={styles.mainUl}>
+                            <li className={styles.mainLists}><Link to="/HomeFeed">홈</Link></li>
+                            <li className={styles.mainLists}><Link to="/Category">랭킹</Link></li>
+                            <li className={styles.mainLists}><Link to="/CelebRecommend">추천</Link></li>
+                            <li className={styles.mainLists}><Link to="/CommunityFeed">커뮤니티</Link></li>
+                            {this.state.isLoggedIn ? (
+                                <li className={styles.mainLists}><Link to="/MypageDefault">마이페이지</Link></li>
+                            ) : (
+                                <li className={styles.mainLists}><Link to="/Login">로그인</Link></li>
+                            )}
+                        </ul>
+                    </nav>
 
-                <nav className={styles.navigation}>
-                    <ul className={styles.mainUl}>
-                        <li className={styles.mainLists}><Link to="/HomeFeed">홈</Link></li>
-                        <li className={styles.mainLists}><Link to="/Category">랭킹</Link></li>
-                        <li className={styles.mainLists}><Link to="/CelebRecommend">추천</Link></li>
-                        <li className={styles.mainLists}><Link to="/CommunityFeed">커뮤니티</Link></li>
-                        <li className={styles.mainLists}><Link to="/MypageDefault">마이페이지</Link></li>
-                    </ul>
-                </nav>
-
-            </div>
-        </header>
-    );
+                </div>
+            </header>
+        );
+    }
 }
 
 export default MainBar;
