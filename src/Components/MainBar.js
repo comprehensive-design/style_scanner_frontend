fcb20d94d5c4f940b3265ad8d0ef2d680a09608b
eunjin <ejejnin@gmail.com>
2024-05-25 13:50:07 +0900

    [Feat] 메뉴바 로그인하지 않은 상태일 때 기능 구현

--- src/Components/MainBar.js
+++ src/Components/MainBar.js
@@ -54,31 +54,49 @@ function MainBar() {
     return (
         <header className={styles.header}>
             <div style={{ display: 'flex' }} className={styles.parent}>
-                <Link to="">
+
+                {isLoggedIn ? (<div><Link to="/HomeFeed">
                     <img
                         src={`img/logo.png`}
                         width='130'
                         height='48.75'
                         alt="Logo"
                     />
-                </Link>
+                </Link></div>) : (
+                    <div>
+                        <Link to="">
+                            <img
+                                src={`img/logo.png`}
+                                width='130'
+                                height='48.75'
+                                alt="Logo"
+                            />
+                        </Link></div>
+                )}
                 <SearchBar value={searchText} onChange={handleSearchChange} onKeyPress={handleKeyPress} />
                 <nav className={styles.navigation}>
                     <ul className={styles.mainUl}>
-                        <li className={styles.mainLists}><Link to="/HomeFeed">홈</Link></li>
-                        <li className={styles.mainLists}><Link to="/Category">랭킹</Link></li>
-                        <li className={styles.mainLists}><Link to="/CelebRecommend">추천</Link></li>
-                        <li className={styles.mainLists}><Link to="/CommunityFeed">커뮤니티</Link></li>
-                        {isLoggedIn ? (
+                        {isLoggedIn ? (<div>
+                            <li className={styles.mainLists}><Link to="/HomeFeed">홈</Link></li>
+                            <li className={styles.mainLists}><Link to="/Category">랭킹</Link></li>
+                            <li className={styles.mainLists}><Link to="/CelebRecommend">추천</Link></li>
+                            <li className={styles.mainLists}><Link to="/CommunityFeed">커뮤니티</Link></li>
                             <li className={styles.mainLists}><Link to="/MypageDefault">마이페이지</Link></li>
+                        </div>
                         ) : (
-                            <li className={styles.mainLists}><Link to="/Login">로그인</Link></li>
+                            <div>
+                                <li className={styles.mainLists}><Link to="/Login">홈</Link></li>
+                                <li className={styles.mainLists}><Link to="/Login">랭킹</Link></li>
+                                <li className={styles.mainLists}><Link to="/Login">추천</Link></li>
+                                <li className={styles.mainLists}><Link to="/Login">커뮤니티</Link></li>
+                                <li className={styles.mainLists}><Link to="/Login">로그인</Link></li>
+                            </div>
                         )}
                     </ul>
                 </nav>
 
             </div>
-        </header>
+        </header >
     );
 }
 
