468841e818d2fbae394f209e6de2f03138fb0e57
wlstj <22020306jinseoyeon@gmail.com>
2024-08-20 00:39:47 +0900

    feat: homeinfo feed 배치

--- src/pages/homeItem/HomeItem.js
+++ src/pages/homeItem/HomeItem.js
@@ -10,10 +10,15 @@ export default function HomeItem() {
     const location = useLocation();
     const { mediaUrls, feedUrl, media_id, username, profile_url, similarImages: initialSimilarImages } = location.state || {};
     const [items, setItems] = useState([]);
-    const [currentPage, setCurrentPage] = useState(0);
-    const itemsPerPage = 4;
+    const [itemsToShow, setItemsToShow] = useState(0);
+    const itemsPerPage = 3;
     const navigate = useNavigate();
     const [similarImages, setSimilarImages] = useState(initialSimilarImages || []);
+
+    const [startIndex, setStartIndex] = useState(0);
+    const thumbnailsToShow = 4;
+
+    //글쓰기 팝업인데, 나중에 navigate로 바꾸면 될듯
     const [isPopupOpen, setIsPopupOpen] = useState(false);
 
     const openPopup = () => {
@@ -24,20 +29,21 @@ export default function HomeItem() {
         setIsPopupOpen(false);
     };
 
-    const nextPage = () => {
-        setCurrentPage((prevPage) => (prevPage + 1) % Math.ceil(similarImages.length / itemsPerPage));
+    const morePage = () => {
+        setItemsToShow((prevItemsToShow) => prevItemsToShow + itemsPerPage);
     };
 
-    const prevPage = () => {
-        setCurrentPage((prevPage) => (prevPage - 1 + Math.ceil(similarImages.length / itemsPerPage)) % Math.ceil(similarImages.length / itemsPerPage));
+    const nextBtn = () => {
+        if (startIndex + thumbnailsToShow < mediaUrls.length - 1) {
+            setStartIndex((prevIndex) => prevIndex + 1);
+        }
     };
-
+    
     useEffect(() => {
         const fetchItemData = async () => {
 
             const token = localStorage.getItem('accessToken');
             if (!token) {
-                alert('로그인이 필요합니다.');
                 return;
             }
 
@@ -66,43 +72,76 @@ export default function HomeItem() {
         }
     }, [initialSimilarImages]);
 
-    const currentItems = items.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage);
+    const currentItems = items.slice(0, itemsToShow);
 
     return (
-        <div className={styles.contents}>
-            {mediaUrls && profile_url && username && media_id && (
-                <Feed
-                    key={media_id}
-                    media_url_list={mediaUrls}
-                    profile_url={profile_url}
-                    username={username}
-                    media_id={media_id}
-                    close={false}
-                />
-            )}
-            <div>
-                <p className={styles.product}>Product</p>
-                <hr></hr>
-                <div className={styles.totalItem}>
-                    {currentItems.map((item, index) => (
-                        <Item
-                            key={item.id}
-                            itemId={item.id}
-                            brand={item.brand}
-                            name={item.name || `Similar Image ${index + 1}`}
-                            price={item.price || 0}
-                            image={item.itemUrl}
-                            shoppingLink={item.shoppingLink}
-                            index={currentPage * itemsPerPage + index}
-                        />
-                    ))}
+        <div>
+            <div className={styles.top}>
+                <div className={styles.titleDiv}>
+                    <div className={styles.title}>아이템 스캔</div>
+                    <div className={styles.subtitle}>
+                        원하는 아이템을 클릭하여, <br />
+                        제품 정보를 찾아봐요</div>
                 </div>
-                <div className={styles.carouselButtons}>
-                    <button className={styles.prevBtn} onClick={prevPage}>{'<'}</button>
-                    <button className={styles.nextBtn} onClick={nextPage}>{'>'}</button>
+                <div className={styles.feedContainer}>
+                    <div className={styles.feedMain}>
+                        {mediaUrls && profile_url && username && media_id && (
+                            <Feed
+                                key={media_id}
+                                media_url_list={mediaUrls}
+                                profile_url={profile_url}
+                                username={username}
+                                media_id={media_id}
+                                close={false}
+                            />
+                        )}
+                    </div>
+                    {mediaUrls.length > 1 && (
+                        <div className={styles.feedSub}>
+                         
+                            {mediaUrls.slice(1).map((url, index) => (
+                                    <img
+                                        key={index}
+                                        src={url}
+                                        alt={`Thumbnail ${index + 1}`}
+                                        className={styles.thumbnail}
+                                    />
+                                ))}
+                            {startIndex + thumbnailsToShow < mediaUrls.length - 1 && (
+                                <button onClick={nextBtn} className={styles.arrowButton}>
+                                    ▾
+                                </button>
+                            )}
+                        </div>
+                    )}
                 </div>
-                <p className={styles.goComBtn} onClick={openPopup}>찾는 제품이 없으신가요?</p>
-                {isPopupOpen && <CommunityWrite feedUrl={feedUrl} onClose={closePopup} />} {/* 팝업 모달 조건부 렌더링 */}
+            </div>
+            <div className={styles.contents}>
+                <div>
+                    <div className={styles.itemTitleDiv}>
+                        <img className={styles.boxIcon} src={`img/boxIcon.png`} />
+                        <div className={styles.itemTitle}>아이템 정보</div>
+                    </div>
+                    <div className={styles.totalItem}>
+                        {currentItems.map((item, index) => (
+                            <ItemInfo
+                                key={item.id}
+                                itemId={item.id}
+                                brand={item.brand}
+                                name={item.name || `Similar Image ${index + 1}`}
+                                price={item.price || 0}
+                                image={item.itemUrl}
+                                shoppingLink={item.shoppingLink}
+                                index={index}
+                            />
+                        ))}
+                    </div>
+                    <div className={styles.btnDiv}>
+                        <button className={styles.moreBtn} onClick={morePage}>더보기</button>
+                        <p className={styles.goComBtn} onClick={openPopup}>찾는 제품이 없으신가요?</p>
+                    </div>
+                    {isPopupOpen && <CommunityWrite feedUrl={feedUrl} onClose={closePopup} />} {/* 팝업 모달 조건부 렌더링 */}
+                </div>
             </div>
         </div>
     );
