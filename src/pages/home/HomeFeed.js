faf31ba155d91b8a890e8834a13b6c1cbdcd3ed1
wlstj <22020306jinseoyeon@gmail.com>
2024-09-09 14:24:18 +0900

    refact: homepage Style.css 적용 변경

--- src/pages/home/HomeFeed.js
+++ src/pages/home/HomeFeed.js
@@ -1,32 +1,12 @@
 import React from 'react';
+import  '../../style/style.css';
 import useHomeFeedLogic from '../../hooks/useHomeFeedLogic'; // Hook을 임포트
-import styled from 'styled-components';
 import Feed from '../../Components/feed/Feed';
 import Loading from '../../Components/loading/loading';
-import { HomeTitleDiv, Title, MainWrapper } from '../../style/commonStyle';
 import { GoHomeFill } from "react-icons/go";
 import Pagination from '../../Components/Pagination';
 import Footer from '../../Components/Footer';
 
-// UI 스타일 정의
-const HomeTitle = styled.h1`
-    ${Title}
-    line-height: 3em;
-    margin-left: 0.5em;
-`;
-
-const FeedScroll = styled.div`
- overflow: hidden; /* 스크롤바 숨기기 */
-`;
-
-const FeedList = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(4, 25em);
-    gap: 2em;
-    justify-content: center;
-    margin: 2em 0;
-`;
 
 const HomeFeed = () => {
     const { feeds, loading, error, feedListRef } = useHomeFeedLogic();
@@ -40,14 +20,14 @@ const HomeFeed = () => {
     }
 
     return (
-        <MainWrapper>
-            <FeedScroll>
-                <HomeTitleDiv>
-                    <GoHomeFill size="3em" />
-                    <HomeTitle>홈</HomeTitle>
-                </HomeTitleDiv>
+        <div className='mainWrapper'>
+            <div className='feedScroll'>
+                <div className='pageTitleDiv mb3'>
+                    <GoHomeFill size="2em" />
+                    <h1 className='title mb105'>홈</h1>
+                </div>
 
-                <FeedList ref={feedListRef}>
+                <div className='feedList' ref={feedListRef}>
                     {feeds.map(feed => (
                         <Feed
                             key={feed.media_id}
@@ -62,11 +42,11 @@ const HomeFeed = () => {
                         />
                     ))}
                     <div style={{ height: '10px' }} />
-                </FeedList>
-            </FeedScroll>
-            <Pagination />
-            <Footer />
-        </MainWrapper>
+                </div>
+            </div>
+            <Pagination/>
+            <Footer/>
+        </div>
     );
 };
 
