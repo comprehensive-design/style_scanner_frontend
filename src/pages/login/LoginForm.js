216d7f54bf627f42cfd03dc3f9bf87e1d5fb8d68
이은진 <intern05@gmail.com>
2024-09-09 15:00:58 +0900

    style.css 공통 색상 설정

--- src/pages/login/LoginForm.js
+++ src/pages/login/LoginForm.js
@@ -8,44 +8,46 @@ export default function Register({
   handleSubmit,
 }) {
   return (
-    <div className="loginContent mt3">
-      <h1 className="title mb05">Style Scanner</h1>
-      <p className="caption mb3"> 당신의 취향을 만들어 드릴게요! </p>
+    <body>
+      <div className="loginContent mt3">
+        <h1 className="title mb05">Style Scanner</h1>
+        <p className="caption mb3"> 당신의 취향을 만들어 드릴게요! </p>
 
-      <form className="loginForm" onSubmit={handleSubmit}>
-        <label className="boldContent" htmlFor="email">
-          이메일 주소
-        </label>
-        <input
-          className="underLine"
-          type="text"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+        <form className="loginForm" onSubmit={handleSubmit}>
+          <label className="boldContent" htmlFor="email">
+            이메일 주소
+          </label>
+          <input
+            className="underLine"
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-        <label className="boldContent" htmlFor="password">
-          비밀번호
-        </label>
-        <input
-          className="underLine"
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <label className="boldContent" htmlFor="password">
+            비밀번호
+          </label>
+          <input
+            className="underLine"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <input className="button mb05" type="submit" value="로그인"></input>
-      </form>
+          <input className="button mb05" type="submit" value="로그인"></input>
+        </form>
 
-      <div
-        className="loginRegister"
-        style={{ textAlign: "right", width: "70%", margin: "0 auto" }}
-      >
-        <NavLink exact={true.toString()} to="/Register">
-          회원가입
-        </NavLink>
+        <div
+          className="loginRegister"
+          style={{ textAlign: "right", width: "70%", margin: "0 auto" }}
+        >
+          <NavLink exact={true.toString()} to="/Register">
+            회원가입
+          </NavLink>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
