9c6e890acad3eb0636e91c3ddb73af7328c4ccc5
wlstj <22020306jinseoyeon@gmail.com>
2024-09-14 21:00:41 +0900

    feat: carousel api 연결

--- src/Components/feed/Feed.js
+++ src/Components/feed/Feed.js
@@ -2,9 +2,46 @@ import React, { useState} from 'react';
 import styled from 'styled-components';
 import FeedPopup from '../FeedPopup';
 import NumberLabel from '../numberLabel';
-import { useFeedLogic } from '../../hooks/feed/useFeedLogic';
-import  '../../style/style.css';
 
+function Feed({ thumbnail_url, profile_url, currentIndex, username, className, handleImageClick, width, height }) {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    const openPopup = () => {
+        setIsPopupOpen(true);
+    };
+
+    const closePopup = () => {
+        setIsPopupOpen(false);
+    };
+
+    return (
+        <FeedDiv className= 'borderRad boxShadow' width={width} height={height}>
+        <ProfileDiv onClick={openPopup}>
+            {isPopupOpen && <FeedPopup onClose={closePopup} user={{ profileName: username }} />}
+            {profile_url ? (
+                <img className='feedProfile' src={profile_url} alt="Profile" />
+            ) : (
+                <img className='feedProfileDefault'/>
+            )}
+            <p className='boldContent'>@{username}</p>
+        </ProfileDiv>
+
+        <FeedMain  width={width}>
+            <img src={thumbnail_url} onClick={handleImageClick}/>
+
+            {/* api수정 후 다시 작성 */}
+            {/* <div className='feedLayerDiv'>
+                {carousel_count > 1 && className==='homefeed' && (
+                    <img src={`img/layer.png`} alt="layer" />
+                )}
+                {carousel_count > 1 && className==='homeitem' && (
+                    <NumberLabel currentPage={currentIndex+1} pageLength={carousel_count} />
+                )}
+            </div> */}
+        </FeedMain>
+    </FeedDiv>
+    );
+}
 //공통 컴포넌트 -> prop을 위해 styled component로
 const FeedDiv = styled.div`
     width: ${({ width }) => width};
@@ -27,45 +64,4 @@ const FeedMain = styled.div`
         height: auto;
     }
 `;
-
-
-function Feed({ key, thumbnail_url, profile_url,  currentIndex, username, feed_code, home, width, height }) {
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
-
-    const openPopup = () => {
-        setIsPopupOpen(true);
-    };
-
-    const closePopup = () => {
-        setIsPopupOpen(false);
-    };
-    // const feedLogic = useFeedLogic({thumbnail_url, currentIndex, username, key, home });
-
-    return (
-        <FeedDiv className= 'borderRad boxShadow' width={width} height={height}>
-        <ProfileDiv onClick={openPopup}>
-            {isPopupOpen && <FeedPopup onClose={closePopup} user={{ profileName: username }} />}
-            {profile_url ? (
-                <img className='feedProfile' src={profile_url} alt="Profile" />
-            ) : (
-                <img className='feedProfileDefault'/>
-            )}
-            <p className='boldContent'>@{username}</p>
-        </ProfileDiv>
-
-        <FeedMain  width={width}>
-            <img src={thumbnail_url} />
-            {/* <div className='feedLayerDiv'>
-                {media_url_list.length > 1 && home && (
-                    <img src={`img/layer.png`} alt="layer" />
-                )}
-                {media_url_list.length > 1 && !home && (
-                    <NumberLabel currentPage={currentIndex+1} pageLength={media_url_list.length} />
-                )}
-            </div> */}
-        </FeedMain>
-    </FeedDiv>
-    );
-}
-
 export default Feed;
--- src/pages/home/HomeFeed.js
+++ src/pages/home/HomeFeed.js
@@ -1,26 +1,50 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import useHomeFeedLogic from '../../hooks/useHomeFeedLogic';
 import Feed from '../../Components/feed/Feed';
 import Loading from '../../Components/loading/loading';
 import { GoHomeFill } from "react-icons/go";
 import Pagination from '../../Components/Pagination';
 import Footer from '../../Components/Footer';
-
+import api from '../../utils/axios'
 
 const HomeFeed = () => {
+    const navigate = useNavigate();
     const [page, setPage] = useState(0);
     const size = 12;
+    const [images, setImages] = useState(null);
 
     const { feeds, loading, error, feedListRef } = useHomeFeedLogic(page, size);
 
     if (loading) {
-        return <Loading />;
+        return <Loading/>;
     }
 
     if (error) {
         return <div>{error}</div>;
     }
-
+    const handleImageClick = async (username, profile_url, feed_code) => {
+        try {
+            const response = await api.get('/api/insta/getCarouselMedia', {
+                params: {
+                    feed_code: feed_code
+                }
+            });
+            console.log(response.data);
+            setImages(response.data);
+            
+        } catch (error) {
+            console.error(error);
+        }
+        
+        navigate("/HomeItem", {
+            state: {
+                mediaUrls: images,
+                username: username,
+                profile_url: profile_url
+            }
+        });
+    };
     return (
         <div className='body'>
             <div className='feedScroll'>
@@ -36,8 +60,8 @@ const HomeFeed = () => {
                             thumbnail_url={feed.thumbnail_url} 
                             profile_url={feed.profile_url} 
                             username={feed.username}
-                            feed_code={feed.feed_code}
-                            home={true}
+                            className={'home'}
+                            handleImageClick={ () => handleImageClick(feed.profile_url, feed.username, feed.feed_code)}
                             currentIndex={0}
                             width="25em"
                             height="35em"
