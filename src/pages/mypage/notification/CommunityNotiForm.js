a4bcb8e0c2a52e14d4fb332c79dc38ce467b8027
eunjin <ejejnin@gmail.com>
2024-09-16 19:09:03 +0900

    [refact] 마이페이지 - 커뮤니티 알림 화면 재구성

--- src/pages/mypage/notification/CommunityNotiForm.js
+++ src/pages/mypage/notification/CommunityNotiForm.js
@@ -1,42 +1,30 @@
-import styles from './CommunityNoti.module.css';
 import Sidebar from '../../../Components/Sidebar';
 import NotiBox from '../../../Components/NotiBox';
 import Pagination from '../../../Components/Pagination';
-import Footer from '../../../Components/Footer';
 
 export default function CommunityNotiForm({ noti, itemsNum, itemsPerPage, setCurrentPage, currentPage }) {
     return (
-        <body>
-            <div className={styles.total}>
-                <Sidebar/>
-                <div className={styles.content}>
-                    <div className={styles.title}>
-                        <h2>알림</h2>
-                        <hr />
-                    </div>
-                    <div className={styles.wrap}>
+        <div className="mypageWrapper">
+            <Sidebar />
+            <div className="mypageMain">
+                <div>
+                    <p className="title left mb05 ml03">알림</p>
+                    <hr />
+                </div>
+                    <div>
                         {noti.length === 0 ? (
                             <p style={{marginTop:"20px"}}>알림이 없습니다.</p>
                         ) : (
                             <NotiBox noti={noti} />
                         )}
-                    </div>
-                </div>
-            </div>
-
-            <div className={styles.heightPadding}></div>
-            <div className={styles.footerBox}>
-                <div className={styles.leftBtween} />
-                <div className={styles.footer}>
+                    </div> 
                     <Pagination
                         itemsNum={itemsNum}
                         itemsPerPage={itemsPerPage}
                         setCurrentPage={setCurrentPage}
                         currentPage={currentPage}
-                    />
-                </div>
+                    /> 
             </div>
-            <Footer />
-        </body>
+        </div>
     );
 }
