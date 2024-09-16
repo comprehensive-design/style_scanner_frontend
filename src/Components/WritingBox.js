784630b71aec29d7ae79055c6d7e0a795c4a97ed
eunjin <ejejnin@gmail.com>
2024-09-16 17:18:26 +0900

    [refact] 마이페이지 - 내가 작성한 글 화면 재구성

--- src/Components/WritingBox.js
+++ src/Components/WritingBox.js
@@ -1,8 +1,3 @@
-import styles from '../css/WritingBox.module.css';
-import Button from './Button';
-import axios from 'axios';
-
-
 const formatDate = (dateArray) => {
     const year = dateArray[0];
     const month = String(dateArray[1]).padStart(2, '0');
@@ -13,26 +8,25 @@ const formatDate = (dateArray) => {
 
 export default function WritingBox({ postId, feedImg, commentCnt, title, date, onDelete, onEdit }) {
        return (
-        <div className={styles.writingBox}>
-            <div className={styles.feedDiv}>
-                {/* src={feedImg} 로 변경해야함 */}
-                <img className={styles.feedImage} src={feedImg}></img>
+        <div className="mppWritingBox">
+            <div  > 
+                <img className="mppFeedImage borderRad" src={feedImg}></img>
             </div>
-            <div className={styles.textDiv}>
-                <p className={styles.title}>{title}</p>
-                {/* <p className={styles.contents}>{content} </p> */}
+            <div>
+            <div className="mt3 ml3">
+                <p className="subtitle">{title}</p> 
             </div>
-            <div className={styles.commentDiv}>
-                <img className={styles.commentImage} src={`img/reply.png`}></img>
-                <p id={styles.commentCnt}>{commentCnt}</p>
+            <div className="mppCommentDiv">
+                <img className="mppCommentImage" src={`img/reply.png`}></img>
+                <p className='ml1'>{commentCnt}</p>
             </div>
-            <div className={styles.dateDiv}>
-                <p className={styles.date}>{formatDate(date)}</p>
             </div>
-            <div className={styles.buttonDiv}>
-                <Button onClick={() => onEdit(postId)} BackColor="#d9d9d9" txtColor='black' border='none' hovColor='black' hovTxtColor='white'>수정</Button>
-                &nbsp;
-                <Button onClick={() => onDelete(postId)} BackColor="#d9d9d9" txtColor='black' border='none' hovColor='black' hovTxtColor='white'>삭제</Button>
+            <div className="mppDateDiv">
+                <p className="caption grayText">{formatDate(date)}</p>
+            </div>
+            <div className="mppButtonDiv">
+                <button className='whiteButton' onClick={() => onEdit(postId)} >수정</button>
+                <button className='whiteButton' onClick={() => onDelete(postId)}>삭제</button>
             </div>
         </div>
     );
