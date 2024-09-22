a68a7a9fd94c7af10c15509b630dffa9537b1fa3
wlstj <22020306jinseoyeon@gmail.com>
2024-09-22 21:21:35 +0900

    refact: comment time n분전 구현

--- src/pages/community/detail/CommunityDetail.js
+++ src/pages/community/detail/CommunityDetail.js
@@ -7,6 +7,7 @@ import { theme } from "../../../style/theme";
 import { IoChatbox } from "react-icons/io5";
 import { FiSend } from "react-icons/fi";
 import Loading from '../../../Components/loading/loading';
+import { DetailTime } from "../../../utils/DetailTime";
 
 export default function CommunityDetail() {
   const commentRef = useRef();
@@ -54,25 +55,7 @@ export default function CommunityDetail() {
     postCreatedAt[4], 
     postCreatedAt[5] 
   );
-
-  const detailDate = (a) => {
-    const milliSeconds = new Date() - a;
-    const seconds = milliSeconds / 1000;
-    if (seconds < 60) return `방금 전`;
-    const minutes = seconds / 60;
-    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
-    const hours = minutes / 60;
-    if (hours < 24) return `${Math.floor(hours)}시간 전`;
-    const days = hours / 24;
-    if (days < 7) return `${Math.floor(days)}일 전`;
-    const weeks = days / 7;
-    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
-    const months = days / 30;
-    if (months < 12) return `${Math.floor(months)}개월 전`;
-    const years = days / 365;
-    return `${Math.floor(years)}년 전`;
-  };
-  const nowDate = detailDate(dateObject);
+  const nowDate = DetailTime(dateObject);
 
   return (
     <div className="body">
@@ -159,6 +142,7 @@ export default function CommunityDetail() {
                   displayName={comment.displayName}
                   content={comment.content}
                   profilePictureUrl={comment.profilePictureUrl}
+                  commentCreatedAt ={comment.createdAt}
                 />
               ))
             ) : (
